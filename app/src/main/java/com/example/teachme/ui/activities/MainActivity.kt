e9be9b6991ec3427ec3921932bfffe6edfd7e03c
Najimkhon <qwerty1995.290@gmail.com>
2023-04-12 05:32:49 +0500

    Fragments are created

--- app/src/main/java/com/example/teachme/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/teachme/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.teachme
+package com.example.teachme.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.teachme.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
