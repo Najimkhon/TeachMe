bac5d73e2635b4354d7af155c6655b001fa4f1a3
Najimkhon <qwerty1995.290@gmail.com>
2023-04-12 16:04:24 +0500

    Bottom navigation is added

--- app/src/main/java/com/example/teachme/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/teachme/ui/activities/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.teachme.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.teachme.R
+import com.example.teachme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        setContentView(view)
+
     }
 }
