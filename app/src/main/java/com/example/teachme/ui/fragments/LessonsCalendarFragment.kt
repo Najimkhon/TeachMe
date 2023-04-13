e401d13bfcbb05b9edcbbce4b83e53cc833981ec
Najimkhon <qwerty1995.290@gmail.com>
2023-04-13 14:53:59 +0500

    Calendar view design applied

--- app/src/main/java/com/example/teachme/ui/fragments/LessonsCalendarFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/LessonsCalendarFragment.kt
@@ -1,10 +1,17 @@
 package com.example.teachme.ui.fragments
 
 
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.Toast
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentLessonsCalendarBinding
 
 
 class LessonsCalendarFragment : BaseFragment<FragmentLessonsCalendarBinding>(FragmentLessonsCalendarBinding::inflate) {
-
+    override fun setListeners() {
+        binding.calendarView.setOnDateChangeListener { view, year, month, day ->
+            val date = day.toString() + "-" + (month + 1) + "-"
+            Toast.makeText(requireContext(), date, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
