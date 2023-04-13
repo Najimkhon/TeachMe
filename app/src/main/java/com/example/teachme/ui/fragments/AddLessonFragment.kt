fbb7ce1e059eff28329ceca8d1a9d17eb5ec5cf1
Najimkhon <qwerty1995.290@gmail.com>
2023-04-13 17:20:14 +0500

    Students page is designed

---
+++ app/src/main/java/com/example/teachme/ui/fragments/AddLessonFragment.kt
@@ -0,0 +1,8 @@
+package com.example.teachme.ui.fragments
+
+import com.example.teachme.base.BaseFragment
+import com.example.teachme.databinding.FragmentAddLessonBinding
+
+
+class AddLessonFragment : BaseFragment<FragmentAddLessonBinding>(FragmentAddLessonBinding::inflate) {
+}
--- app/src/main/java/com/example/teachme/ui/fragments/LessonsCalendarFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/LessonsCalendarFragment.kt
@@ -1,8 +1,8 @@
 package com.example.teachme.ui.fragments
 
 
-import android.widget.CalendarView.OnDateChangeListener
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentLessonsCalendarBinding
 
@@ -10,8 +10,10 @@ import com.example.teachme.databinding.FragmentLessonsCalendarBinding
 class LessonsCalendarFragment : BaseFragment<FragmentLessonsCalendarBinding>(FragmentLessonsCalendarBinding::inflate) {
     override fun setListeners() {
         binding.calendarView.setOnDateChangeListener { view, year, month, day ->
-            val date = day.toString() + "-" + (month + 1) + "-"
+            val date = day.toString() + "-" + (month + 1)
             Toast.makeText(requireContext(), date, Toast.LENGTH_SHORT).show()
+            val action = LessonsFragmentDirections.actionLessonsFragmentToAddLessonFragment()
+            findNavController().navigate(action)
         }
     }
 }
