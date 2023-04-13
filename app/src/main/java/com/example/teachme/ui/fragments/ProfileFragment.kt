99afb69f89525b7f6235fc582bb8d5e9519d175a
Najimkhon <qwerty1995.290@gmail.com>
2023-04-14 00:21:42 +0500

    Profile page is designed

--- app/src/main/java/com/example/teachme/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/ProfileFragment.kt
@@ -4,5 +4,7 @@ import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentProfileBinding
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
-
+    override fun prepareUI() {
+        binding.toolbar.tvTitle.text = "Profile"
+    }
 }
