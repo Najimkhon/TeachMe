486531c9c6ba4a022cf9cb0cc48fa5b33c96a55c
Najimkhon <qwerty1995.290@gmail.com>
2023-04-14 17:03:29 +0500

    Main functions are added

--- app/src/main/java/com/example/teachme/repositories/MainRepository.kt
+++ app/src/main/java/com/example/teachme/repositories/MainRepository.kt
@@ -1,9 +1,18 @@
 package com.example.teachme.repositories
 
+import androidx.lifecycle.LiveData
 import com.example.teachme.data.db.StudentDAO
+import com.example.teachme.data.models.StudentPM
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     val studentDAO: StudentDAO
 ) {
+
+    suspend fun insertStudent(studentPM: StudentPM) = studentDAO.insertStudent(studentPM)
+
+    suspend fun deleteStudent(studentPM: StudentPM) = studentDAO.deleteStudent(studentPM)
+
+    fun getAllStudents() = studentDAO.getAllStudents()
+
 }
--- app/src/main/java/com/example/teachme/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/teachme/ui/viewmodels/MainViewModel.kt
@@ -1,12 +1,25 @@
 package com.example.teachme.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.teachme.data.models.StudentPM
 import com.example.teachme.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    val students = mainRepository.getAllStudents()
+
+    fun insertStudent(studentPM: StudentPM) = viewModelScope.launch {
+        mainRepository.insertStudent(studentPM)
+    }
+
+    fun deleteStudent(studentPM: StudentPM) = viewModelScope.launch {
+        mainRepository.deleteStudent(studentPM)
+    }
 }
