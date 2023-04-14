80c1db280a2583120c280300f7f9eb885d81030a
Najimkhon <qwerty1995.290@gmail.com>
2023-04-14 21:54:14 +0500

    BottomnavigationBar color is set

--- app/src/main/java/com/example/teachme/ui/fragments/IntroFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/IntroFragment.kt
@@ -1,8 +1,20 @@
 package com.example.teachme.ui.fragments
 
+import com.example.teachme.R
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentIntroBinding
 
 class IntroFragment : BaseFragment<FragmentIntroBinding>(FragmentIntroBinding::inflate) {
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().window.navigationBarColor = resources.getColor(R.color.black)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requireActivity().window.navigationBarColor = resources.getColor(R.color.darker_white)
+    }
+
 
 }
