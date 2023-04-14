0fdfcb517d3952b5d34b81c3532ee25887e579eb
Najimkhon <qwerty1995.290@gmail.com>
2023-04-14 12:16:37 +0500

    Sign in page is designed

--- app/src/main/java/com/example/teachme/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/HomeFragment.kt
@@ -1,9 +1,19 @@
 package com.example.teachme.ui.fragments
 
+
+import androidx.navigation.fragment.findNavController
+import com.example.teachme.R
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentHomeBinding
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
+    override fun setListeners() {
+        binding.cvExpense.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_introFragment)
+        }
+        binding.cvIncome.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_tutorSignInFragment)
+        }
+    }
 }
