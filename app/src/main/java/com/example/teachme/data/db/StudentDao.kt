9bf3248993616ca4aaf4c03a8bea7b5938651d62
Najimkhon <qwerty1995.290@gmail.com>
2023-04-14 16:41:54 +0500

    Create StudentDao.kt

---
+++ app/src/main/java/com/example/teachme/data/db/StudentDao.kt
@@ -0,0 +1,22 @@
+package com.example.teachme.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.teachme.data.models.StudentPM
+
+@Dao
+interface StudentDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(studentPM: StudentPM)
+
+    @Delete
+    suspend fun deleteStudent(studentPM: StudentPM)
+
+    @Query("SELECT * FROM student_table ORDER BY id DESC")
+    fun getAllStudents(): LiveData<List<StudentPM>>
+}
