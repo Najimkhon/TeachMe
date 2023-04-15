26135c827d84bae5397b77485a5b6edf3d9e7b74
Najimkhon <qwerty1995.290@gmail.com>
2023-04-15 15:28:58 +0500

    Lesson funs added to MainViewModel

--- app/src/main/java/com/example/teachme/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/teachme/ui/viewmodels/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.teachme.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.teachme.data.models.LessonPM
 import com.example.teachme.data.models.StudentPM
 import com.example.teachme.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,6 +15,7 @@ class MainViewModel @Inject constructor(
 ): ViewModel() {
 
     val students = mainRepository.getAllStudents()
+    val lessons = mainRepository.getAllLessons()
 
     fun insertStudent(studentPM: StudentPM) = viewModelScope.launch {
         mainRepository.insertStudent(studentPM)
@@ -21,5 +23,13 @@ class MainViewModel @Inject constructor(
 
     fun deleteStudent(studentPM: StudentPM) = viewModelScope.launch {
         mainRepository.deleteStudent(studentPM)
+    }
+
+    fun insertLesson(lessonPM: LessonPM) = viewModelScope.launch {
+        mainRepository.insertLesson(lessonPM)
+    }
+
+    fun deleteLesson(lessonPM: LessonPM) = viewModelScope.launch {
+        mainRepository.deleteLesson(lessonPM)
     }
 }
