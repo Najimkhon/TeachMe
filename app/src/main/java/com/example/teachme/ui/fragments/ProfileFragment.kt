11d7612c911777a83efa81b6a53e269a4f9a1f0a
Najimkhon <qwerty1995.290@gmail.com>
2023-04-16 23:40:53 +0500

    Navigate to introFragment when Logout is clicked

--- app/src/main/java/com/example/teachme/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/ProfileFragment.kt
@@ -1,10 +1,18 @@
 package com.example.teachme.ui.fragments
 
+import androidx.navigation.fragment.findNavController
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentProfileBinding
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
     override fun prepareUI() {
         binding.toolbar.tvTitle.text = "Profile"
+    }
+
+    override fun setListeners() {
+        binding.btnLogOut.setOnClickListener{
+            val action = ProfileFragmentDirections.actionProfileFragmentToIntroFragment()
+            findNavController().navigate(action)
+        }
     }
 }
