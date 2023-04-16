b41938f74aa5cb9c38805696f8e499637ac8e11a
Najimkhon <qwerty1995.290@gmail.com>
2023-04-16 21:16:20 +0500

    Enrolled Students are displayed in LessonInfo

--- app/src/main/java/com/example/teachme/ui/fragments/LessonInfoFragment.kt
+++ app/src/main/java/com/example/teachme/ui/fragments/LessonInfoFragment.kt
@@ -3,11 +3,15 @@ package com.example.teachme.ui.fragments
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.teachme.R
 import com.example.teachme.base.BaseFragment
 import com.example.teachme.databinding.FragmentLessonInfoBinding
+import com.example.teachme.ui.adapters.StudentAdapter
 import com.example.teachme.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,6 +19,7 @@ import java.util.*
 class LessonInfoFragment :
     BaseFragment<FragmentLessonInfoBinding>(FragmentLessonInfoBinding::inflate) {
     private val viewModel: MainViewModel by viewModels()
+    private val studentAdapter: StudentAdapter by lazy { StudentAdapter(requireContext()) }
     private val args by navArgs<LessonInfoFragmentArgs>()
     private val formatter = SimpleDateFormat("MMMM dd, EEEE", Locale.US)
     private val timeFormatter = SimpleDateFormat("HH:mm", Locale.US)
@@ -29,7 +34,16 @@ class LessonInfoFragment :
                 etSubject.text = it.title
                 cbGenerate.isChecked = it.autogenerateLessons
                 setSelectedDays(it.selectedDays)
+                studentAdapter.setData(it.students)
             }
+        }
+    }
+
+    override fun assignObjects() {
+        binding.rvSlectedStudents.apply {
+            adapter = studentAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            itemAnimator = SlideInUpAnimator().apply { addDuration = 300 }
         }
     }
 
