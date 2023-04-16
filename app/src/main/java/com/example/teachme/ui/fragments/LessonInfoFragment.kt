283b55dbe024633cd87dd2af5ef1a8270407434c
Najimkhon <qwerty1995.290@gmail.com>
2023-04-16 20:21:39 +0500

    Basic LessonInfo fragment is designed

---
+++ app/src/main/java/com/example/teachme/ui/fragments/LessonInfoFragment.kt
@@ -0,0 +1,10 @@
+package com.example.teachme.ui.fragments
+
+import com.example.teachme.base.BaseFragment
+import com.example.teachme.databinding.FragmentLessonInfoBinding
+
+class LessonInfoFragment :
+    BaseFragment<FragmentLessonInfoBinding>(FragmentLessonInfoBinding::inflate) {
+
+
+}
