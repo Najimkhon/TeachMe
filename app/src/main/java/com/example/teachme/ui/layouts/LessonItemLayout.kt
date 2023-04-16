86aab32cd9df51c8b433f3406c48d3ef07adb8ae
Najimkhon <qwerty1995.290@gmail.com>
2023-04-16 18:18:27 +0500

    Lesson item layout is designed

--- app/src/main/java/com/example/teachme/ui/layouts/LessonItemLayout.kt
+++ app/src/main/java/com/example/teachme/ui/layouts/LessonItemLayout.kt
@@ -4,16 +4,27 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import com.example.teachme.data.models.LessonPM
-import com.example.teachme.data.models.StudentPM
 import com.example.teachme.databinding.LessonItemBinding
-import com.example.teachme.databinding.StudentItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LessonItemLayout(context: Context) :
     RelativeLayout(context) {
 
-        private val binding = LessonItemBinding.inflate(LayoutInflater.from(context), this, true)
+    private val binding = LessonItemBinding.inflate(LayoutInflater.from(context), this, true)
+    private val timeFormatter = SimpleDateFormat("HH:mm", Locale.US)
 
-    fun fillContent(lessonPM: LessonPM){
-        binding.lessonTitle.text = lessonPM.title
+    fun fillContent(lessonPM: LessonPM) {
+
+        val startTime = timeFormatter.format(lessonPM.startTime)
+        val endTime = timeFormatter.format(lessonPM.endTime)
+        val studentCount = lessonPM.students.size
+
+        binding.apply {
+            tvTime.text = "$startTime-$endTime"
+            lessonTitle.text = lessonPM.title
+            tvRate.text = lessonPM.rate.name
+            tvStudentsCount.text = "$studentCount Students"
+        }
     }
 }
